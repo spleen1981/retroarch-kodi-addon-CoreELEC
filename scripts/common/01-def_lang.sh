41ccc079596d5f1db6d85f00240cb369dbeadbc8
Michael Martins <michaelfm21@gmail.com>
2023-07-16 21:33:59 -0300

    Update 01-def_lang.sh

--- scripts/common/01-def_lang.sh
+++ scripts/common/01-def_lang.sh
@@ -19,7 +19,7 @@ msgstr ""
 "Language: %s\\\\n"
 "Plural-Forms: nplurals=2; plural=(n != 1)\\\\n"\\n'
 
-LANG_list="en_gb cs_cz it_it zh_cn sk_sk"
+LANG_list="en_gb cs_cz it_it zh_cn sk_sk pt_br"
 LANG_max=32013
 
 LANG_0_ctx="Addon Summary"
@@ -28,6 +28,7 @@ LANG_0_cs_cz="Doplněk RetroArch pro Kodi (${RA_NAME_SUFFIX}). RetroArch je fron
 LANG_0_it_it="RetroArch add-on per Kodi (${RA_NAME_SUFFIX}). RetroArch è un frontend per emulatori, giochi e media player."
 LANG_0_sk_sk="Doplnok RetroArch pre Kodi (${RA_NAME_SUFFIX}). RetroArch je frontend pre emulátory, herné enginy a prehrávače médií."
 LANG_0_zh_cn="Kodi (${RA_NAME_SUFFIX}) 的 RetroArch 附加组件。RetroArch 是一个模拟器、游戏引擎和媒体播放器的前端。"
+LANG_0_pt_br="RetroArch add-on para Kodi (${RA_NAME_SUFFIX}). RetroArch é um frontend para emuladores, motores de jogos e reprodutores de mídia."
 
 LANG_1_ctx="Addon Description"
 LANG_1_en_gb="The add-on provides binary, cores and basic settings to launch RetroArch from Kodi UI, plus additional features to improve user experience. It is built from Lakka sources."
@@ -35,6 +36,7 @@ LANG_1_cs_cz="Doplněk poskytuje jádra a základní nastavení pro spuštění 
 LANG_1_it_it="Questa add-on include i binari, i core e i settaggi di base per eseguire RetroArch dalla UI di Kodi, più funzionalità aggiuntive per migliorare l'esperienza utente. È costruito dai sorgenti di Lakka."
 LANG_1_sk_sk="Doplnok poskytuje jadrá a základné nastavenia pre spustenie RetroArch z rozhrania Kodi a dodatočné funkcie pre zlepšenie používateľského zážitku. Je postavený na zdrojoch Lakka."
 LANG_1_zh_cn="此附加组件从 Lakka 源码直接构建，同时提供了二进制，内核，从 Kodi UI 启动 RetroArch 的基本设置，以及改善用户体验的附加功能。"
+LANG_1_pt_br="O add-on fornece binário, núcleos e configurações básicas para iniciar o RetroArch a partir da UI do Kodi, além de recursos adicionais para melhorar a experiência do usuário. Ele é construído a partir das fontes do Lakka."
 
 LANG_2_ctx="Addon Disclaimer"
 LANG_2_en_gb="This is an unofficial add-on. Use github.com/spleen1981/retroarch-kodi-addon-CoreELEC to submit issues."
@@ -42,63 +44,74 @@ LANG_2_cs_cz="Toto je neoficiální doplněk. K odeslání problémů použijte:
 LANG_2_it_it="Questa è una add-on non ufficiale. Usa github.com/spleen1981/retroarch-kodi-addon-CoreELEC per segnalare eventuali problemi."
 LANG_2_sk_sk="Toto je neoficiálny doplnok. Na nahlásenie problémov použite github.com/spleen1981/retroarch-kodi-addon-CoreELEC."
 LANG_2_zh_cn="这是一个非官方的附加组件。使用 github.com/spleen1981/retroarch-kodi-addon-CoreELEC 提交问题。"
+LANG_2_pt_br="Este é um add-on não oficial. Use github.com/spleen1981/retroarch-kodi-addon-CoreELEC para enviar problemas."
 
 LANG_32002_en_gb="Turn off Xbox360 controllers after closing RetroArch"
 LANG_32002_cs_cz="Po vypnutí RetroArch vypněte ovladače Xbox360"
 LANG_32002_it_it="Spegni i controller Xbox360 all'uscita di RetroArch"
 LANG_32002_sk_sk="Po vypnutí RetroArch vypnite ovládače Xbox360"
 LANG_32002_zh_cn="在关闭 RetroArch 时关闭 Xbox360 控制器"
+LANG_32002_pt_br="Desligar controles do Xbox360 depois de fechar o RetroArch"
 
 LANG_32003_en_gb="Use remote control (CEC) with RetroArch"
 LANG_32003_cs_cz="Použijte dálkové ovládání (CEC) s Retroarch"
 LANG_32003_it_it="Usa telecomando (CEC) con Retroarch"
 LANG_32003_sk_sk="Použite diaľkové ovládanie (CEC) s Retroarch"
 LANG_32003_zh_cn="在 RetroArch 中使用 CEC 控制"
+LANG_32003_pt_br="Usar controle remoto (CEC) com o RetroArch"
 
 LANG_32004_en_gb="Override Kodi refresh rate settings"
 LANG_32004_cs_cz="Přepsat nastavení obnovovací frekvence Kodi"
 LANG_32004_it_it="Ignora frequenza di aggiornamento impostata in Kodi"
 LANG_32004_sk_sk="Prepísať nastavenia obnovovacej frekvencie Kodi"
 LANG_32004_zh_cn="覆盖 Kodi 的刷新率设置"
+LANG_32004_pt_br="Substituir as configurações da taxa de atualização do Kodi"
 
 LANG_32005_en_gb="RetroArch refresh rate"
 LANG_32005_cs_cz="Obnovovací frekvence RetroArch"
 LANG_32005_it_it="Frequenza di aggiornamento RetroArch"
 LANG_32005_sk_sk="Obnovovacia frekvencia RetroArch"
 LANG_32005_zh_cn="RetroArch 刷新率"
+LANG_32005_pt_br="Taxa de atualização do RetroArch"
 
 LANG_32006_en_gb="Sync RetroArch audio driver/device with Kodi"
 LANG_32006_cs_cz="Synchronizovat zvukový ovladač/zařízení RetroArch s Kodi"
 LANG_32006_it_it="Sincronizza impostazioni driver/device audio di RetroArch con Kodi"
 LANG_32006_sk_sk="Synchronizovať zvukový ovládač/zariadenie RetroArch s Kodi"
 LANG_32006_zh_cn="将 RetroArch 音频驱动程序/设备与 Kodi 同步"
+LANG_32006_pt_br="Sincronizar o driver/dispositivo de áudio do RetroArch com o Kodi"
 
 LANG_32007_en_gb="Mount remote path for RetroArch ROMs"
 LANG_32007_cs_cz="Připojit vzdálenou cestu pro ROMs RetroArch"
 LANG_32007_it_it="Monta percorso remoto per le ROM di RetroArch"
 LANG_32007_sk_sk="Pripojiť vzdialenú cestu pre ROMky RetroArch"
 LANG_32007_zh_cn="为 RetroArch ROMs 挂载远程路径"
+LANG_32007_pt_br="Montar caminho remoto para as ROMs do RetroArch"
 
 LANG_32008_en_gb="Save RetroArch logs to file"
 LANG_32008_cs_cz="Uložit protokoly RetroArch do souboru"
 LANG_32008_it_it="Salva i log di RetroArch su file"
 LANG_32008_sk_sk="Uložiť protokoly RetroArch do súboru"
 LANG_32008_zh_cn="将 RetroArch 日志保存到文件"
+LANG_32008_pt_br="Salvar registros do RetroArch para arquivo"
 
 LANG_32009_en_gb="Turn off BT controllers on RetroArch exit (if supported)"
 LANG_32009_it_it="Spegni i controller BT all'uscita di RetroArch (se supportato)"
 LANG_32009_sk_sk="Vypnúť BT ovládače pri vypnutí RetroArch-u (ak je podporované)"
 LANG_32009_zh_cn="在 RetroArch 退出时关闭蓝牙控制器（如果支持的话）"
+LANG_32009_pt_br="Desligar os controles BT controllers ao sair do RetroArch (se suportado)"
 
 LANG_32010_en_gb="Boot the system to"
 LANG_32010_it_it="Avvia il sistema con"
 LANG_32010_sk_sk="Naštartovať systém do"
 LANG_32010_zh_cn="启动系统时打开"
+LANG_32010_pt_br="Iniciar o sistema em"
 
 LANG_32011_en_gb="Currently the system boots to"
 LANG_32011_it_it="Il sistema attualmente si avvia con"
 LANG_32011_sk_sk="Systém aktuálne štartuje do"
 LANG_32011_zh_cn="当前启动系统时会打开"
+LANG_32011_pt_br="Atualmente o sistema inicializa em"
 
 LANG_32012_en_gb="Do you want to change the setting and boot to"
 LANG_32012_it_it="Vuoi cambiare l'impostazione e avviare con"
